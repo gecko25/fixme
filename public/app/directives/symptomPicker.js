5516f0811eb539f32ebd59476f5f1aded37e7466
saratankard <stankard@thelathe.com>
2016-04-07 11:41:53 -0400

    Getting symptom picker to work

--- public/app/directives/symptomPicker.js
+++ public/app/directives/symptomPicker.js
@@ -2,8 +2,8 @@ angular.module('fixme').directive('fmSymptomPicker', function($http){
     return {
         templateUrl: 'app/templates/symptomPicker.html',
         restrict: 'E',
-        controller: function($scope){
-            $scope.symptoms = [];
+        controller: function($scope, $q){
+            //on page startup, get data
             $http({
                 method: 'GET',
                 url: '/api/symptoms'
@@ -15,6 +15,40 @@ angular.module('fixme').directive('fmSymptomPicker', function($http){
                 console.log(response);
             });
 
-        }
+            //on text change in autocomplete
+
+            $scope.searchSymptoms = function(searchText){
+                var re = new RegExp('^' + searchText, "i");
+
+                var deferred = $q.defer();
+
+
+                console.log('Searching with regex:')
+                console.log(re);
+
+                try{
+                    var matches = [];
+
+                    $scope.symptoms.forEach( (symptom) => {
+                        //search the array for matching expressions
+                        if (re.test(symptom.name)){
+                            matches.push(symptom);
+                            deferred.resolve(matches);
+                        }
+                    });
+
+
+                }catch(e){
+                    deferred.reject(null);
+                }
+
+                //return matches;
+                return deferred.promise;
+            }
+
+
+        } 
     }
 })
+
+
